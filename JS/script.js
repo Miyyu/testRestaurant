a2beb066bc028c497e4c32073c3fade71dac9bc0
Emily Santos <emily.mimy12@gmail.com>
2021-03-13 22:30:35 -0300

    Removendo aside

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,3 @@
-var tela = window.document.getElementById("corpo");
-
-tela.addEventListener("scroll", fixarMenu);
-
-function fixarMenu(){
-
-    console.log("DESCEU");
-
+function girarImagens(){
+    
 }
