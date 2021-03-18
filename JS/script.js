b4eeebe9a271c8fffd80d52590e458544ddf720d
Emily Santos <emily.mimy12@gmail.com>
2021-03-18 16:13:34 -0300

    Arrumando caixas flutuantes

--- JS/script.js
+++ JS/script.js
@@ -1,42 +1,24 @@
+var blocos = window.document.querySelectorAll(".imagensMenu");
+for(var i = 0; i < blocos.length; i++){
+    blocos[i].addEventListener("mouseenter", function(){
+        console.log("Em cima");
+        
+        var saberConteudo = this.alt;
 
-    
-    var blocos = window.document.querySelectorAll(".imagensMenu");
-    for(var i = 0; i < blocos.length; i++){
-        blocos[i].addEventListener("mouseenter", function(){
-            console.log("Em cima");
-            
-            var b = this.alt;
+        /*console.log(b);*/
 
-            console.log(b);
-            let nome = window.document.querySelector("#bebida");
-            if(b === "Bebidas"){
-                nome.innerHTML = "DRINK";
-            }
-        })
-    }
-    /*let nome = window.document.querySelector(".textoEmCima");
-    switch(nome.id){
-        case "comida":
+        if(saberConteudo === "Prato de Entrada"){
+            let nome = window.document.querySelector("#comida");
             nome.innerHTML = "COMIDAS";
-            break;
-        case "bebida":
-            nome.innerHTML = "BEBIDAS";
-            break;
-        case "sobremesa":
+        }
+        
+        if(saberConteudo === "Bebidas"){
+            let nome = window.document.querySelector("#bebida");
+            nome.innerHTML = "DRINKS";
+        }
+        else if (saberConteudo === "Sobremesas"){
+            let nome = window.document.querySelector("#sobremesa");
             nome.innerHTML = "SOBREMESAS";
-            break;
-    }*/
-
-    /*if(nome.id === "comida"){
-        nome.innerHTML = "COMIDAS";
-    }
-    else if(nome.id === "bebidas"){
-        nome.innerHTML = "BEBIDAS";
-    }*/
-    
-
-
-/*function saiu(imagem){
-    console.log("saiu");
-    var nome = window.document.querySelector(".textoEmCima").innerHTML = "";
-}*/
+        }
+    })
+}
