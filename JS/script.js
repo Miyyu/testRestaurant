f1adbad5763c0132e635fb63ee771a92631d4a00
Emily Santos <emily.mimy12@gmail.com>
2021-03-20 22:46:57 -0300

    ADD comentários no JS

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,16 @@
 var blocos = window.document.querySelectorAll(".imagensMenu");
-for(var i = 0; i < blocos.length; i++){
-    blocos[i].addEventListener("mouseenter", function(){
-        console.log("Em cima");
+for(var i = 0; i < blocos.length; i++){ //uso um for para iterar entre as imagens para saber em qual estou com o mouse em cima
+    blocos[i].addEventListener("mouseenter", function(){ //criando function para caso alguma imagem tenho o mouse em cima
+        console.log("Em cima"); //Verifico se o mouse está em cima da img
         
-        var saberConteudo = this.alt;
+        var saberConteudo = this.alt; //Pegando o valor do alt das img do HTML
 
         if(saberConteudo === "Prato de Entrada"){
             let nome = window.document.querySelector("#comida");
             nome.innerHTML = "COMIDAS";
         }
         
-        if(saberConteudo === "Bebidas"){
+        else if(saberConteudo === "Bebidas"){
             let nome = window.document.querySelector("#bebida");
             nome.innerHTML = "DRINKS";
         }
